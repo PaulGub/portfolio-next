b45146ad27fae5a61f39fdbcbf9dbca71017f960
Paul Gubbiotti <paul.gubbiotti@gmail.com>
2022-12-04 12:34:07 +0100

    responsive v1

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,11 @@ import logo from '../images/roi-lion.gif'
 
 export default function Home() {
   return (
-    <div className="bg-cover bg-blue-300 h-screen w-full flex flex-col items-center justify-center">
+    <div className="bg-cover bg-blue-300 h-screen w-full flex flex-col items-center justify-center p-4">
 
-    <h1 className="text-9xl p-9"> Bienvenue sur gubbiotti.fr </h1>
+    <h1 className="text-4xl lg:text-8xl lg:p-9 p-2"> Bienvenue sur gubbiotti.fr </h1>
 
-    <p className="p-9">Site en cours de construction, bientôt sur vos écrans !</p>
+    <p className="lg:p-9 p-2">Site en cours de construction, bientôt sur vos écrans !</p>
 
     <Image src={logo} width="500" height="500" alt="logo" />
 
