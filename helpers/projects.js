2f26cabfa6f949fcee1783de6bb173b45ca03e3b
Paul Gubbiotti <paul.gubbiotti@gmail.com>
2024-03-26 09:33:42 +0100

    Update projects.js

--- helpers/projects.js
+++ helpers/projects.js
@@ -1,13 +1,14 @@
 export const projects = [
-    {
-        linkGitHub: "https://github.com/PaulGub/portfolio-next",
-        link: "https://www.gubbiotti.fr/",
-        img: "/projects/paul.png",
-        title: "Mon portfolio",
-        description: "Mon site portfolio est un espace en ligne où je présente mes compétences, mes réalisations et mes projets passés. Il est conçu pour mettre en valeur mes talents et me permettre de me connecter avec d'éventuels employeurs ou clients potentiels.",
-        techno: ["NextJS", "TailwindCSS"],
-    },
-    /*
+  {
+    linkGitHub: "https://github.com/PaulGub/portfolio-next",
+    link: "https://www.gubbiotti.fr/",
+    img: "/projects/paul.png",
+    title: "Mon portfolio",
+    description:
+      "Mon site portfolio est un espace en ligne où je présente mes compétences, mes réalisations et mes projets passés. Il est conçu pour mettre en valeur mes talents et me permettre de me connecter avec d'éventuels employeurs ou clients potentiels.",
+    techno: ["NextJS", "TailwindCSS"],
+  },
+  /*
     {
         linkGitHub: "https://github.com/PaulGub/angevie-next",
         link: "https://www.angeetvie.fr/",
@@ -17,41 +18,45 @@ export const projects = [
         techno: ["WordPress"],
     },
     */
-    {
-        linkFigma: "https://www.figma.com/file/XlTEMQET0PNQDr5nsp8Uzb/Nnuno?type=design&node-id=3-99&t=OGNGDH2lqJ3P9B4U-0",
-        linkAPI: "https://cms.nnuno.org/wp-json/api",
-        link: "https://nnuno.org/",
-        img: "/projects/nnuno.png",
-        title: "Nnuno.org",
-        description: "J'ai orchestré la refonte du design de ce site.",
-        techno: ["NextJS","TailwindCSS", "WordPress"],
-    },
-    {
-        link: "http://puyravault17.3codes.io/",
-        img: "/projects/puyravault.png",
-        title: "Puyravault",
-        description: "Ce site est actuellement en phase finale de développement.",
-        techno: ["WordPress"],
-    },
-    {
-        link: "https://www.fimewc.fr/fr",
-        img: "/projects/fimewc.png",
-        title: "FIM EWC",
-        description: "J'ai contribué de manière significative à la conception de sections clés de ce site.",
-        techno: ["NextJS", "TailwindCSS", "GraphQL"],
-    },
-    {
-        link: "https://www.vallishabitat.fr/",
-        img: "/projects/vallishabitat.png",
-        title: "Vallis Habitat",
-        description: "J'ai contribué de manière significative à la conception de sections clés de ce site.",
-        techno: ["WordPress"],
-    },
-    {
-        link: "https://ucitrackchampionsleague.com/fr/",
-        img: "/projects/ucitrack.png",
-        title: "UCI Track Champions League",
-        description: "J'ai contribué de manière significative à la conception de sections clés de ce site.",
-        techno: ["WordPress"],
-    }
+  {
+    linkFigma:
+      "https://www.figma.com/file/XlTEMQET0PNQDr5nsp8Uzb/Nnuno?type=design&node-id=3-99&t=OGNGDH2lqJ3P9B4U-0",
+    linkAPI: "https://cms.nnuno.org/wp-json/api",
+    link: "https://nnuno.org/",
+    img: "/projects/nnuno.png",
+    title: "Nnuno.org",
+    description: "J'ai orchestré la refonte du design de ce site.",
+    techno: ["NextJS", "TailwindCSS", "WordPress"],
+  },
+  {
+    link: "https://www.puyravault17.fr/",
+    img: "/projects/puyravault.png",
+    title: "Puyravault",
+    description: "Ce site est actuellement en phase finale de développement.",
+    techno: ["WordPress"],
+  },
+  {
+    link: "https://www.fimewc.fr/fr",
+    img: "/projects/fimewc.png",
+    title: "FIM EWC",
+    description:
+      "J'ai contribué de manière significative à la conception de sections clés de ce site.",
+    techno: ["NextJS", "TailwindCSS", "GraphQL"],
+  },
+  {
+    link: "https://www.granddelta.fr/",
+    img: "/projects/vallishabitat.png",
+    title: "Vallis Habitat",
+    description:
+      "J'ai contribué de manière significative à la conception de sections clés de ce site.",
+    techno: ["WordPress"],
+  },
+  {
+    link: "https://ucitrackchampionsleague.com/fr/",
+    img: "/projects/ucitrack.png",
+    title: "UCI Track Champions League",
+    description:
+      "J'ai contribué de manière significative à la conception de sections clés de ce site.",
+    techno: ["WordPress"],
+  },
 ];
